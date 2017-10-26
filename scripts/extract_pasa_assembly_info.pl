8780d96eacfad4a07a48789a2578908a37532202
Jose Gonzalez <jmg@sanger.ac.uk>
2017-10-26 13:46:50 +0100

    Library name is obtained from the file name by default

--- scripts/extract_pasa_assembly_info.pl
+++ scripts/extract_pasa_assembly_info.pl
@@ -37,6 +37,9 @@ foreach my $lib_file (@lib_files){
 		$lib_name = "SLR-seq_".$1;
 		$lib_name =~ s/-DNA_A01-LRAAD-01//;
 	}
+	else{
+	  ($lib_name) = $lib_file =~ /\/(\w+)\.gff3(\.gz)?/;
+	}
 	
 	if ($lib_file =~ /\.gz/){
 		open (LIB, "zcat $lib_file | ") or die "Can't open file $lib_file: $!";
