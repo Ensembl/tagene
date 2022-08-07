63ddb04437e9380e79b15d31d9af2125dbbb6a7e
jmgonzmart <jmgonzalez@ebi.ac.uk>
2022-08-07 12:55:11 +0100

    Report outcome by input transcript using the farm output files

---
+++ scripts/report_by_transcript.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+for ID in `cat $1`; do
+  IDSTR=${ID}":"
+  OUTCOME=$(cat $@ | grep ^TR2: | grep -w $IDSTR | cut -d':' -f4)
+  FILTER=
+  if [[ "${OUTCOME}" =~ "intron did not pass the filter" ]]; then
+    FILTER=$(cat $@ | grep -B3 ^TR2: | grep -B3 -w $IDSTR | head -1)
+  elif [[ "${OUTCOME}" =~ "as intron chain exists" ]]; then
+    OUTCOME=" "$(cat $@ | grep -B1 ^TR: | grep -B1 -w $IDSTR | head -1 | cut -d'-' -f2)
+  fi    
+  echo -e $ID"\t"$OUTCOME"\t"$FILTER
+done
