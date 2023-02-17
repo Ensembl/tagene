bebd2669c12bfa3d2f0da1a99b360e5d1bb7058b
jmgonzmart <jmgonzalez@ebi.ac.uk>
2023-02-17 19:57:03 +0000

    Catch output message for skipped partially redundant transcripts

--- scripts/report_by_transcript.sh
+++ scripts/report_by_transcript.sh
@@ -8,7 +8,10 @@ for ID in `cat $1`; do
   if [[ "${OUTCOME}" =~ "intron did not pass the filter" ]]; then
     FILTER=$(cat $@ | grep -B3 ^TR2: | grep -B3 -w $IDSTR | head -1)
   elif [[ "${OUTCOME}" =~ "as intron chain exists" ]]; then
-    OUTCOME=" "$(cat $@ | grep -B1 ^TR: | grep -B1 -w $IDSTR | head -1 | cut -d'-' -f2)
+    OUTCOME2=" "$(cat $@ | grep -B1 ^TR: | grep -B1 -w $IDSTR | head -1 | cut -d'-' -f2)
+    if [[ "${OUTCOME2}" =~ "Skipping transcript as partially redundant" ]]; then
+      OUTCOME=$OUTCOME2
+    fi
   fi    
   echo -e $ID"\t"$OUTCOME"\t"$FILTER
 done
