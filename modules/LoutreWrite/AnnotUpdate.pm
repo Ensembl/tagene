ad70575aad42e131aaa4bfffdf6bd6f74884ef2f
jmgonzmart <jmgonzalez@ebi.ac.uk>
2023-05-22 14:50:08 +0100

    Allow adding multi-exon transcripts to existing single-exon genes

--- modules/LoutreWrite/AnnotUpdate.pm
+++ modules/LoutreWrite/AnnotUpdate.pm
@@ -180,9 +180,10 @@ print "SUBMODE: $submode\n";
                     }
                     #Exclude transcripts having a "comp_pipe_rejected" remark
                     next if scalar(grep {$_->value eq "comp_pipe_rejected"} @{$db_tr->get_all_Attributes('remark')});
-                    #Ignore single-exon transcripts unless TAGENE transcript is single-exon too (?)
+                    #Ignore single-exon transcripts unless TAGENE transcript is single-exon too?
+                    #No: for the moment, just allow adding multi-exon transcripts to existing single-exon genes         
                     if (scalar @{$tr->get_all_Exons} > 1){
-                      next if scalar @{$db_tr->get_all_Exons} == 1;  #Do not merge with single-exon transcripts? (In case they are CLS Platinum...)
+                  #    next if scalar @{$db_tr->get_all_Exons} == 1;  #Do not merge with single-exon transcripts? (In case they are CLS Platinum...)
                     }
 
                     my $i = intron_novelty($tr, $db_tr);
