4505f9e4e0b5d45a28ee7f7a2d1ff13f4d4b0083
jmgonzmart <jmgonzalez@ebi.ac.uk>
2024-02-02 17:42:52 +0000

    Fixed bug in genePredToBigGenePred command

--- scripts/convert_gtf_to_bigGenePred.pl
+++ scripts/convert_gtf_to_bigGenePred.pl
@@ -38,8 +38,10 @@ my $meta_container = $db->get_MetaContainerAdaptor();
 my $assembly_version = $meta_container->single_value_by_key('assembly.ucsc_alias');
 
 system("gtfToGenePred -genePredExt $gtf_file file.gp");
-system("genePredToBigGenePred file.gp file.bgpInput");
-system("wget https://hgdownload.soe.ucsc.edu/goldenPath/$assembly_version/bigZips/$assembly_version.chrom.sizes");
+system("genePredToBigGenePred file.gp stdout > file.bgpInput");
+unless (-e "$assembly_version.chrom.sizes"){
+  system("wget https://hgdownload.soe.ucsc.edu/goldenPath/$assembly_version/bigZips/$assembly_version.chrom.sizes");
+}
 check_chromosome_names("file.bgpInput", "$assembly_version.chrom.sizes");
 system("LC_COLLATE=C sort -k1,1 -k2,2n file.bgpInput.2 > file.bgpInput");
 print_bgp_definition_file("bigGenePred.as");
